ca1883664a2f3ae25cc137b17330e31e0328e2d6
Luis Hernandez <Estudiante@DESKTOP-J6D5H3A>
2026-01-29 10:12:11 -0600

    Desarrollo de la interfaz grafica de la calculadora

--- miPrimeraApp/app/build.gradle.kts
+++ miPrimeraApp/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.ugb.miprimeraapp"
-        minSdk = 22
+        minSdk = 23
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
