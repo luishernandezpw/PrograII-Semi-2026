1142fb5e7ec64583b5b8c9df58724b518c45a492
Luis Hernandez <sarco_luis@hotmail.com>
2026-02-06 09:50:34 -0600

    Creacion de la rama conversores

--- miPrimeraApp/app/src/main/java/com/ugb/miprimeraapp/MainActivity.java
+++ miPrimeraApp/app/src/main/java/com/ugb/miprimeraapp/MainActivity.java
@@ -13,50 +13,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-/*
-Tarea para viernes 06/02/2026 individual.
-Factorial
-Porcentaje
-Exponenciacion
-Raiz
-* */
 public class MainActivity extends AppCompatActivity {
-    TextView tempVal;
-    Button btn;
-    Spinner spn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.btnCalcular);
-        btn.setOnClickListener(v->calcular());
-    }
-    private void calcular(){
-        tempVal = findViewById(R.id.txtNum1);
-        Double num1 = Double.parseDouble(tempVal.getText().toString());
-
-        tempVal = findViewById(R.id.txtNum2);
-        Double num2 = Double.parseDouble(tempVal.getText().toString());
-
-        double respuesta = 0;
-
-        spn = findViewById(R.id.cboOpciones);
-        switch (spn.getSelectedItemPosition()){
-            case 0: //suma
-                respuesta = num1 + num2;
-                break;
-            case 1: //Resta
-                respuesta = num1 - num2;
-                break;
-            case 2: //Multiplicacion
-                respuesta = num1 * num2;
-                break;
-            case 3: //division
-                respuesta = num1 / num2;
-                break;
-        }
-        tempVal = findViewById(R.id.lblRespuesta);
-        tempVal.setText("Respuesta: "+ respuesta);
     }
 }
