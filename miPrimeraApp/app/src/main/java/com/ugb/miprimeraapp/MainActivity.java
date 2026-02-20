64f711ea3152cd6c6608c003b3b7b427570b25af
Luis Hernandez <sarco_luis@hotmail.com>
2026-02-20 10:07:16 -0600

    correcion de la rama sensor de LUZ

--- miPrimeraApp/app/src/main/java/com/ugb/miprimeraapp/MainActivity.java
+++ miPrimeraApp/app/src/main/java/com/ugb/miprimeraapp/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sensorAcelerometro();
+        sensorLuz();
     }
     private void iniciar(){
         sensorManager.registerListener(sensorEventListener, sensor, 2000*1000);
@@ -48,22 +48,30 @@ public class MainActivity extends AppCompatActivity {
     private void detener(){
         sensorManager.unregisterListener(sensorEventListener);
     }
-    private void sensorAcelerometro(){
-        tempVal = findViewById(R.id.lblSensorAcelerometro);
+    private void sensorLuz(){
+        tempVal = findViewById(R.id.lblSensorLuz);
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         if(sensor==null){
-            tempVal.setText("No dispones del sensor de acelerometro");
+            tempVal.setText("No dispones del sensor de Luz");
             finish();
         }
         sensorEventListener = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent sensorEvent) {
-                double x = sensorEvent.values[0];
-                double y = sensorEvent.values[1];
-                double z = sensorEvent.values[2];
-                tempVal.setText("Desplazamiento: X: "+ x +"\n; Y: "+y +"\n; Z: "+z);
-
+                double valor = sensorEvent.values[0];
+                tempVal.setText("Luz: "+ valor);
+                int color = Color.BLACK;
+                if(valor>=0 && valor<=50){
+                    color = Color.GRAY;
+                }
+                if(valor>=51 && valor<=100){
+                    color = Color.YELLOW;
+                }
+                if (valor>=0 && valor<150){
+                    color = Color.BLUE;
+                }
+                getWindow().getDecorView().setBackgroundColor(color);
             }
             @Override
             public void onAccuracyChanged(Sensor sensor, int i) {
