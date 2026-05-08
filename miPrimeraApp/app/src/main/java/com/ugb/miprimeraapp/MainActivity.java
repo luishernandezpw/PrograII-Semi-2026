54261f6e78e573e3adaa19e3a640b7e1d0515619
Luis Hernandez <sarco_luis@hotmail.com>
2026-05-08 09:33:07 -0600

    Creacion de la rama mensajeria

--- miPrimeraApp/app/src/main/java/com/ugb/miprimeraapp/MainActivity.java
+++ miPrimeraApp/app/src/main/java/com/ugb/miprimeraapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ugb.miprimeraapp;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -13,50 +14,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-/*
-Tarea para viernes 06/02/2026 individual.
-Factorial
-Porcentaje
-Exponenciacion
-Raiz
-* */
-public class MainActivity extends AppCompatActivity {
-    TextView tempVal;
-    Button btn;
-    Spinner spn;
+public class MainActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.btnCalcular);
-        btn.setOnClickListener(v->calcular());
-    }
-    private void calcular(){
-        tempVal = findViewById(R.id.txtNum1);
-        Double num1 = Double.parseDouble(tempVal.getText().toString());
-
-        tempVal = findViewById(R.id.txtNum2);
-        Double num2 = Double.parseDouble(tempVal.getText().toString());
-
-        double respuesta = 0;
-
-        spn = findViewById(R.id.cboOpciones);
-        switch (spn.getSelectedItemPosition()){
-            case 0: //suma
-                respuesta = num1 + num2;
-                break;
-            case 1: //Resta
-                respuesta = num1 - num2;
-                break;
-            case 2: //Multiplicacion
-                respuesta = num1 * num2;
-                break;
-            case 3: //division
-                respuesta = num1 / num2;
-                break;
-        }
-        tempVal = findViewById(R.id.lblRespuesta);
-        tempVal.setText("Respuesta: "+ respuesta);
     }
 }
